5f48df609d0bf049579581b07666e29620b6decc
akasakacmp <akasaka@c-m-p.jp>
2017-07-13 16:43:57 +0900

    20170713

---
+++ common/js/common.js
@@ -0,0 +1,18 @@
+// JavaScript Document
+var winHeight = $(window).innerHeight();
+var winWidth = $(window).innerWidth();
+var noScroll = '.noScrl';//カンマ区切りで除外するクラスを設定
+
+$(function(){
+	
+  //スムーススクロール
+  $('a[href^="#"]').not(noScroll).click(function() {
+    var speed = 400;
+    var href= $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $('body,html').animate({scrollTop:position}, speed, 'swing');
+    return false;
+  });
+  
+});
