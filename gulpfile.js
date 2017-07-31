e7a54cb6156ab69916868a808d3b6d34e5a1c6ed
akasakacmp <akasaka@c-m-p.jp>
2017-07-31 15:30:05 +0900

    2017073104_諸々構成変更

--- gulpfile.js
+++ gulpfile.js
@@ -8,44 +8,60 @@ var uglify = require('gulp-uglify');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
 var mozjpeg  = require('imagemin-mozjpeg');
+var changed  = require('gulp-changed');
 var connect = require('gulp-connect');
 var plumber = require('gulp-plumber');
 var notify  = require('gulp-notify');
 
+//ディレクトリ設定
+var dir = {
+  src : '_src', //作業用ディレクトリ
+  dist : 'dist' //納品用ディレクトリ
+}
+
+//scssの設定
 gulp.task('sass', function() {
-  gulp.src('./common/scss/*.scss')
+  gulp.src(dir.src + '/{,**/}*.scss') //作業対象
     .pipe(plumber({
-      errorHandler: notify.onError("Error: <%= error.message %>")
+      errorHandler: notify.onError('Error: <%= error.message %>')
     }))
-    .pipe(sass())
-    .pipe(autoprefixer())
-    .pipe(cleanCSS())
-    .pipe(gulp.dest('./common/dist/css'));
+    .pipe(sass()) //cssに変換
+    .pipe(autoprefixer()) //ベンダープレフィックス付与
+    .pipe(cleanCSS()) //コード縮小
+    .pipe(gulp.dest(dir.src)) //作業用ディレクトリにcss書き出し
+    .pipe(gulp.dest(dir.dist)) //納品用ディレクトリにcss書き出し
+    .pipe(connect.reload());  //ブラウザリロード
 });
 
+//aigisの設定
 gulp.task('aigis', function() {
-  return gulp.src('./aigis_config.yml')
+  return gulp.src('./aigis_config.yml') //aigis設定ファイル（読み込み対象とかここのファイルで指定）
     .pipe(plumber({
-      errorHandler: notify.onError("Error: <%= error.message %>")
+      errorHandler: notify.onError('rror: <%= error.message %>')
     }))
     .pipe(aigis());
 });
 
+//jsの設定
 gulp.task('minify-js', function() {
-  return gulp.src("./common/js/*.js")
+  return gulp.src(dir.src + '/{,**/}*.js') //作業対象
     .pipe(plumber({
-      errorHandler: notify.onError("Error: <%= error.message %>")
+      errorHandler: notify.onError('Error: <%= error.message %>')
     }))
-    .pipe(uglify())
-    .pipe(gulp.dest('./common/dist/js'));
+    .pipe(uglify()) //コード縮小
+    .pipe(gulp.dest(dir.dist)); //納品用ディレクトリにcss書き出し
 });
 
+//imageの設定
 gulp.task('imagemin', function() {
-  return gulp.src('./common/images/*')
+  var srcGlob = dir.src + '/{,**/}*.+(jpg|jpeg|png|gif|svg)'; //作業用ディレクトリ
+  var dstGlob = dir.dist; //納品用ディレクトリ
+  return gulp.src(srcGlob) //作業対象
     .pipe(plumber({
-      errorHandler: notify.onError("Error: <%= error.message %>")
+      errorHandler: notify.onError('Error: <%= error.message %>')
     }))
-    .pipe(imagemin([
+    .pipe(changed(dstGlob)) //作業用ディレクトリのファイルが納品用ディレクトリのファイルより新しいか確認
+    .pipe(imagemin([ //オプションに合わせて画像圧縮
        pngquant({
          quality: '65-80',
          speed: 1,
@@ -60,18 +76,43 @@ gulp.task('imagemin', function() {
        imagemin.gifsicle()
      ]
   ))
-  .pipe(gulp.dest('./common/dist/images'));
+  .pipe(gulp.dest(dstGlob)); //納品用ディレクトリにimage書き出し
 });
 
+//htmlファイルコピー
+gulp.task('copy', function () {
+  return gulp.src(dir.src + '/{,**/}*.html') //作業用ディレクトリ
+    .pipe(gulp.dest( dir.dist )) //納品用ディレクトリにhtml書き出し
+    .pipe(connect.reload());  //ブラウザリロード
+});
+
+//localhost:8080を有効化
 gulp.task('connect', function() {
   connect.server({
-    root: './',
+    root: './dist', //ローカルサーバー立ち上げ時のルートディレクトリ
     livereload: true
   });
 });
 
-gulp.task('default', ['sass', 'aigis', 'minify-js', 'imagemin', 'connect'], function(){
-  gulp.watch('./common/scss/*.scss', ['sass', 'aigis']);
-  gulp.watch('./common/js/*.js', ['minify-js']);
-  gulp.watch('./common/images/', ['imagemin']);
+//監視対象を設定
+gulp.task('watch', () => {
+  return gulp.src([dir.src + '/{,**/}*.scss'], function () {
+    return gulp.start(['sass', 'aigis']);
+  });
+  return gulp.src([dir.src + '/{,**/}*.js'], function () {
+    return gulp.start(['minify-js']);
+  });
+  return gulp.src([dir.src + '/{,**/}*.+(jpg|jpeg|png|gif|svg)'], function () {
+    return gulp.start(['imagemin']);
+  });
+  return gulp.src([dir.src + '/{,**/}*.html'], function () {
+    return gulp.start(['copy']);
+  });
+});
+
+gulp.task('default', ['sass', 'aigis', 'minify-js', 'imagemin', 'copy', 'connect', 'watch'], function(){
+  gulp.watch(dir.src + '/{,**/}*.scss', ['sass', 'aigis']);
+  gulp.watch(dir.src + '/{,**/}*.js', ['minify-js']);
+  gulp.watch(dir.src + '/{,**/}*.html', ['copy']);
+  gulp.watch(dir.src + '/{,**/}*.+(jpg|jpeg|png|gif|svg)', ['imagemin']);
 });
