b54dca4e5c335fb2a390dbe4936ffe8e40f40d00
akasakacmp <akasaka@c-m-p.jp>
2017-07-31 10:31:10 +0900

    2017073101_gulp-uglify_gulp-clean-css追加

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,28 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require("gulp-autoprefixer");
+var cleanCSS = require('gulp-clean-css');
 var aigis = require('gulp-aigis');
+var uglify = require('gulp-uglify');
 var connect = require('gulp-connect');
 
 gulp.task('sass', function() {
   gulp.src('./common/scss/*.scss')
     .pipe(sass())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('./common/css'));
+    .pipe(cleanCSS())
+    .pipe(gulp.dest('./common/dist/css'));
 });
 
 gulp.task('aigis', function() {
   return gulp.src('./aigis_config.yml')
     .pipe(aigis());
+});
+
+gulp.task('minify-js', function() {
+  return gulp.src("./common/js/*.js")
+    .pipe(uglify())
+    .pipe(gulp.dest('./common/dist/js'));
 });
 
 gulp.task('connect', function() {
@@ -24,6 +33,7 @@ gulp.task('connect', function() {
   });
 });
 
-gulp.task('default', ['sass', 'aigis', 'connect'], function(){
+gulp.task('default', ['sass', 'aigis', 'minify-js', 'connect'], function(){
   gulp.watch('./common/scss/*.scss', ['sass', 'aigis']);
+  gulp.watch('./common/js/*.js', ['minify-js']);
 });
