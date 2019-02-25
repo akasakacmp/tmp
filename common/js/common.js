3fbac3c52e21134a90221c931b1f31b2b6e2c32a
akasakacmp <akasaka@c-m-p.jp>
2019-02-25 17:25:00 +0900

    テンプレート更新

--- common/js/common.js
+++ common/js/common.js
@@ -5,6 +5,7 @@ var noScroll = '.noScrl';//カンマ区切りで除外するクラスを設定
 
 $(document).ready(function() {
   proc_scroll();
+  fixed_menu();
   hamburger_menu();
   pop_image();
   accordion();
@@ -25,6 +26,12 @@ var proc_scroll = function() {
   });
 }
 
+var fixed_menu = function() {
+  //headmenuをfixedするときの#pageHead高さ調整
+  var menuHeight = $('#pageHead .fixed').innerHeight();
+  $('#pageHead').css({height: menuHeight});
+}
+
 var hamburger_menu = function() {
   //ハンバーガーメニュー
   $('.hamburger').click(function () {
@@ -39,16 +46,26 @@ var pop_image = function() {
   $('.popImage').click(function(){
     var href = $(this).attr('href');
     var cap = $(this).data('title');
+    var popImage_no_cap = $('<div id="popImage" style="display: none;"><div class="inner"><img src="' + href + '"></div></div>');
+    var popImage_cap = $('<div id="popImage" style="display: none;"><div class="inner"><img src="' + href + '"><div class="popCaption">'+ cap +'</div></div></div>')
     if (cap == undefined) {
-      $('body').prepend('<div id="popImage"><div class="inner"><img src="' + href + '"></div></div>');
+      $('body').prepend(popImage_no_cap);
+      popImage_no_cap.ready(function(){
+        $(popImage_no_cap).fadeIn();
+      })
     } else {
-      $('body').prepend('<div id="popImage"><div class="inner"><img src="' + href + '"><div class="popCaption">'+ cap +'</div></div></div>');
+      $('body').prepend(popImage_cap);
+      popImage_cap.ready(function(){
+        $(popImage_cap).fadeIn();
+      });
     }
     return false;
   });
   //拡大画像閉じる
   $(document).on('click','#popImage',function(){
-    $('#popImage').remove();
+    $('#popImage').fadeOut().queue(function(){
+      $('#popImage').remove().dequeue();
+    });
   });
 }
 
